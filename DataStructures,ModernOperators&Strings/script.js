f4c852b0cf8a9de16bb09c22bb718395ab6dd981
Aman Kumar Goswami <goswamiamankumar24@gmail.com>
2025-06-07 15:11:14 +0530

    Destructuring

--- DataStructures,ModernOperators&Strings/script.js
+++ DataStructures,ModernOperators&Strings/script.js
@@ -4,10 +4,36 @@ const restaurant = {
     categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
     starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
     mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    openingHours: {
+        thu: {
+            open: 12,
+            close: 22,
+        },
+        fri: {
+            open: 11,
+            close: 23,
+        },
+        sat: {
+            open: 0, // Open 24 hours
+            close: 24,
+        },
+    },
     order: function (starterIndex, mainIndex) {
         return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-    }
-}
+    },
+    orderDelivery: function ({ starterIndex, time, mainIndex, address }) {
+        console.log(`Order received! ${restaurant.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered shortly at ${address} on ${time}`);
+    },
+
+};
+restaurant.orderDelivery
+    ({
+        time: '18:30',
+        address: 'Via Del Sole,21',
+        starterIndex: 2,
+        mainIndex: 2,
+    })
+
 const arr = [1, 2, 3];
 const a = arr[0];
 const b = arr[1];
@@ -44,3 +70,26 @@ console.log(i, j, k);
 
 const [p = 1, q = 1, r = 1] = [4, 5];
 console.log(p, q, r);
+
+//Destructuring of Objects
+
+const { name, categories, openingHours } = restaurant;
+console.log(name, categories, openingHours);
+
+const { categories: types, location: venue, mainMenu: menu } = restaurant;
+console.log(types, venue, menu);
+
+const { food = [], location: place = [] } = restaurant;
+console.log(food, place);
+
+let s = 441;
+let t = 224;
+const obj = { s: 22, t: 12, u: 98 };
+console.log(s, t);
+({ s, t } = obj);
+console.log(s, t);
+
+const { fri: { open: o, close: cl } } = openingHours;
+console.log(o, cl);
+
+
