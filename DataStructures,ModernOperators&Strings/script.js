27691e07b387fe10b7b881b614fba88eaf33e9a7
Aman Kumar Goswami <goswamiamankumar24@gmail.com>
2025-06-12 01:08:28 +0530

    Operators

--- DataStructures,ModernOperators&Strings/script.js
+++ DataStructures,ModernOperators&Strings/script.js
@@ -256,3 +256,30 @@ for (const aman of menuu)
 for (const aman of menuu.entries())
     // console.log(aman);
     console.log(`${aman[0] + 1}:${aman[1]} `)
+
+
+if (restaurant.openingHours && restaurant.openingHours.mon)
+    console.log(restaurant.openingHours.mon.open);
+
+//WITH Optional Chaining
+
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+const days = ['Mon', 'Tue', 'Wed', 'thu', 'fri', 'sat', 'sun'];
+
+for (const day of days) {
+    console.log(day);
+    const open = restaurant.openingHours[day]?.open ?? 'closed';
+    console.log(open);
+    console.log(`On ${day}, we open at ${open}`);
+}
+//METHODS//
+
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist.');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist.');
+
+//Arrays//
+
+const users = [{ naam: 'Jonas', email: 'jonas@gmail.co' }];
+console.log(users[0]?.naam ?? 'User Array Empty')
