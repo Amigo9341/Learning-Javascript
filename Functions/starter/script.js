39eeec2bb276805624589343da0df7576d654681
Aman Kumar Goswami <goswamiamankumar24@gmail.com>
2025-06-27 01:14:54 +0530

    Functions

--- Functions/starter/script.js
+++ Functions/starter/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const bookings = [];
+
+//ES6 method of setting default parameters
+const createBooking = function (flightNum,
+    numPassengers = 1,
+    price = 199 * numPassengers + '$') {
+    // ES5 method
+    // numPassengers = numPassengers || 1;
+    // price = price || '199$'
+
+    const booking = {
+        flightNum,
+        numPassengers,
+        price
+    }
+    console.log(booking);
+    bookings.push(booking);
+}
+createBooking('AL148', 202, '372$');
+createBooking('PJ249', 135, '380$');
+createBooking('SL151', 3);
+createBooking('KS151', undefined, '365$');
+createBooking('XZ151', 10, undefined);
