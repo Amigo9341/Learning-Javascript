d810af8ed7f6f7d8e308200caeb5bad1f76fa202
Aman Kumar Goswami <goswamiamankumar24@gmail.com>
2026-02-19 00:03:20 +0530

    hola

--- Arrays-Bankist/starter/script.js
+++ Arrays-Bankist/starter/script.js
@@ -71,7 +71,7 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -107,3 +107,25 @@ console.log([...arr, ...arr2]);
 
 //JOIN
 console.log(letters.join(' - '));
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+for (const movement of movements) {
+
+  if (movement > 0) {
+    console.log(`You deposited ${movement}`);
+  }
+  else {
+    console.log(`You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+console.log('----FOR EACH----');
+movements.forEach(function (movement) {
+  if (movement > 0) {
+    console.log(`You deposited ${movement}`);
+  }
+  else {
+    console.log(`You withdrew ${Math.abs(movement)}`);
+  }
+});
