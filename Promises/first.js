39e1bc4957e6bc6a4f5c10b9ccde5034ca79f1ab
Aman Kumar Goswami <goswamiamankumar24@gmail.com>
2026-08-12 00:13:54 +0530

    Promisesss

--- Promises/first.js
+++ Promises/first.js
@@ -31,15 +31,15 @@
 
 //CREATING PROMIMSES
 
-const A1 = new Promise((resolve, reject) => {
-    reject("Hello");
-})
+// const A1 = new Promise((resolve, reject) => {
+//     reject("Hello");
+// })
 
-A1.then((answer) => {
-    console.log(answer)})
-    .catch((wrong) => {
-        console.log(wrong)
-    })
+// A1.then((answer) => {
+//     console.log(answer)})
+//     .catch((wrong) => {
+//         console.log(wrong)
+//     })
 
 // const j1 = {
 //     name: "Aman",
@@ -58,3 +58,92 @@ A1.then((answer) => {
 // }`                                              //JSON exists in String format. That's why "`" is used.
 // const JSObj = JSON.parse(j2);
 // console.log(JSObj);
+
+//CALLBACK HELL IMPROVISING
+
+const orderDetail = {
+    orderId : 121236,
+    food : ["Pizza", "Biryani", "Coke"],
+    cost : 610,
+    customerName : "Rohit",
+    customerLocation : "Dwarka",
+    restaurantLocation : "Delhi"
+}
+
+function placedOrder(orderDetail){
+    console.log(`Rs. ${orderDetail.cost} Payment is in progress`);
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if(Math.random() > 0.05) {
+                console.log("Payment is received and order is now placed.")
+        orderDetail.status = true;
+        resolve(orderDetail)}
+        else{
+            reject("Payment is failed");
+        }
+    }, 3000);
+    })
+    
+    
+}
+
+function preparingOrder(orderDetail){
+    console.log(`Your food preparation is now started of ${orderDetail.food}`);
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if(Math.random > 0.9){
+            console.log("Food is now out for delivery!")
+        orderDetail.token = 123;
+        resolve(orderDetail)}
+        else{
+            reject("Food is not prepared yet!")
+        }
+    }, 5000);
+    })   
+}
+
+function pickupOrder(orderDetail){
+    console.log(`Delivery Boy is on the way to pickup the order from ${orderDetail.restaurantLocation}.`);
+
+    return new Promise((resolve, reject) =>{
+        setTimeout(() => {
+            if(Math.random > 0.9){
+                console.log("I have picked up the order.")
+        orderDetail.received = true;
+        resolve(orderDetail)}
+        else{
+        reject("Order cannot be picked!")
+    }
+    }, 5000)
+    
+    })    
+}
+
+function deliverOrder(orderDetail){
+    console.log(`I am on my way to deliver order ${orderDetail.customerLocation}.`);
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if(Math.random > 0.9) {
+                console.log("Order delivered SUCCESSFULLY!!!")
+    orderDetail.delivery = true;
+    resolve(orderDetail)}
+    else{
+        reject("Order cannot be delivered!!")
+    }
+    }, 5000)
+    })
+}
+
+
+placedOrder(orderDetail)
+.then((orderDetail) => preparingOrder(orderDetail))
+.then((orderDetail) => pickupOrder(orderDetail))
+.then((orderDetail) => deliverOrder(orderDetail))
+.then((orderDetail) => {
+    console.log(orderDetail);
+})
+.catch((error) => {console.log("ERROR ",error)
+});
