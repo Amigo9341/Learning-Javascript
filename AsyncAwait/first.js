94ba97e3b2a38ad656acf0a303e685e440f193c8
Aman Kumar Goswami <goswamiamankumar24@gmail.com>
2026-08-14 23:18:49 +0530

    yessss

--- AsyncAwait/first.js
+++ AsyncAwait/first.js
@@ -16,23 +16,45 @@
 //     console.log("Error: ", error)
 // })
 
-fetch("https://api.github.com/users")
-.then((data) => data.json())
-.then((output) => console.log(output));
+// fetch("https://api.github.com/users")
+// .then((data) => data.json())
+// .then((output) => console.log(output));
 
-const data = await fetch("https://api.github.com/users");             //fetch is an asynchronous function, will take time to execute. JS wont wait, will move to next line
-const output = await data.json();                                     //this is also an asynchronous function, JS will throw error
-console.log(output);                                            //Using await will stop JS to goto next line until the task isnt finished.
+// const data = await fetch("https://api.github.com/users");             //fetch is an asynchronous function, will take time to execute. JS wont wait, will move to next line
+// const output = await data.json();                                     //this is also an asynchronous function, JS will throw error
+// console.log(output);                                            //Using await will stop JS to goto next line until the task isnt finished.
 
-console.log("yooooo");                                          //This await can freeze everything i.e., until the task isnt complete, JS wont move.
+// console.log("yooooo");                                          //This await can freeze everything i.e., until the task isnt complete, JS wont move.
 
 async function github() {
     console.log("Hello World")                                  //Synchronous task hai to perform ho jayega
     const data = await fetch("https://api.github.com/users");    //Ab idhar same Web API ko dene wala kaam ho jayega and JS move forward krega. 
     const output = await data.json(); 
-    console.log(output); 
+    // console.log(output); 
+
+
+    const parent = document.getElementById("first")
+
+    for(let user of output) {
+
+        const element = document.createElement("div");
+        element.classList.add("user");
+
+        const image = document.createElement("img");
+        image.src = user.avatar_url;
+
+        const userName = document.createElement("h2");
+        userName.textContent = user.login;
+
+        const anchor = document.createElement("a");
+        anchor.href = user.html_url;
+        anchor.textContent = "Visit Profile";
+
+        element.append(image,userName,anchor);
+        parent.append(element);
+    }
     
 }
 
 github();
-console.log("yooooo");  
+// console.log("yooooo");  
